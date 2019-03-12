24e3173311b3ebe4d6a9d374c0291dda6f7db1f4
Gabriel Tay <gabrieltaykl@gmail.com>
2019-03-12 16:43:44 +0800

    Docker service for Postgres

---
+++ docker/services.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+export POSTGRES_HOST="localhost"
+
+function cleanup() {
+    docker-compose -f docker/services.yml down
+}
+trap cleanup EXIT
+
+docker-compose -f docker/services.yml up
